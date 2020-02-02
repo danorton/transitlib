7ed9f283e55fe15195f7ad6bc4df413911a0c2f9
danorton <daniel@danielnorton.com>
2020-02-02 15:12:59 -0600

    misc cleanup

--- src/build/pre-compile.sh
+++ src/build/pre-compile.sh
@@ -1,4 +1,5 @@
-echo "BEGIN pre-compile ***********************"
+#!/usr/bin/env bash
+# © 2020 Daniel Norton
+echo "[INFO] ---------------------------- [ pre-compile ] ---------------------------"
 #env
 protoc submodules/google/transit/gtfs-realtime/proto/gtfs-realtime.proto --java_out=src/main/java
-echo "  END pre-compile ***********************"
--- src/main/java/com/weirdocomputing/transitlib/VehiclePositionUpdate.java
+++ src/main/java/com/weirdocomputing/transitlib/VehiclePositionUpdate.java
@@ -1,18 +1,13 @@
 package com.weirdocomputing.transitlib;
 
 import com.google.transit.realtime.GtfsRealtime;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
-import java.io.IOException;
-import java.io.InputStream;
+
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 /**
