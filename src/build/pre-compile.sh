5c0a14335c884caee60223e58c6002a2e57eba54
danorton <daniel@danielnorton.com>
2020-02-03 09:58:40 -0600

    pull google sources at specific hash (v1.0)

--- src/build/pre-compile.sh
+++ src/build/pre-compile.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
+set -e
 # © 2020 Daniel Norton
 echo "[INFO] ---------------------------- [ pre-compile ] ---------------------------"
 #env
-protoc submodules/google/transit/gtfs-realtime/proto/gtfs-realtime.proto --java_out=src/main/java
+user=google
+repo=transit
+tag=33c329b0d07e72744de43319f839810a5df0b439
+tarball=/tmp/$user-$repo-$tag.tgz
+if [ ! -d src/main/java/com/$user/$repo ]; then
+  echo "[INFO] Fetching google/transit source"
+  curl --output $tarball -L https://github.com/$user/$repo/archive/$tag.tar.gz
+  tar xzf $tarball
+  protoc $repo-$tag/gtfs-realtime/proto/gtfs-realtime.proto --java_out=src/main/java
+  rm -f $tarball
+fi
